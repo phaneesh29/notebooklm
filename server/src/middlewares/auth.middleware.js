4e83a6501f960f0df7882ea7ea2754c051277aae
phaneesh29 <kanugovisreephaneesha@gmail.com>
2026-03-30 19:33:05 +0530

    feat: add API key validation middleware and implement group detail page with document management and streaming chat

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -4,10 +4,11 @@ import { db } from '../db/index.js';
 import { users } from '../db/schema.js';
 import ApiError from '../utils/ApiError.js';
 import { decrypt } from '../utils/encryption.js';
+import { getAuth } from '@clerk/express';
 
 export const requireApiKey = async (req, res, next) => {
   try {
-    const { userId } = req.auth;
+    const { userId } = getAuth(req);
     if (!userId) {
       throw new ApiError(httpStatus.UNAUTHORIZED, 'You must be logged in');
     }
